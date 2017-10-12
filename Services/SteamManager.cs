353e11761bb268ebc3bb19f5ed35b669c3a520c0
mteodorescu <mteodorescu@EN614592>
2017-10-12 11:52:44 +0300

    Add steam manager

---
+++ Services/SteamManager.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using Services.Model;
+using System;
+using System.Net;
+
+namespace Services
+{
+    public static class SteamManager
+    {
+        private const string API_URL = "http://store.steampowered.com/api/appdetails?appids={0}";
+
+        /// <summary>
+        /// Load game by id
+        /// </summary>
+        /// <param name="steamId"></param>
+        /// <returns></returns>
+        public static SteamModel LoadGameById(string steamId)
+        {
+            if (string.IsNullOrWhiteSpace(steamId))
+            {
+                throw new ArgumentNullException(nameof(steamId));
+            }
+
+            using(WebClient cl = new WebClient())
+            {
+                Uri uri = new Uri(string.Format(API_URL, steamId));
+
+                string data = cl.DownloadStringTaskAsync(uri).Result;
+
+                return JsonConvert.DeserializeObject<SteamModel>(data);
+            }
+        }
+    }
+}
