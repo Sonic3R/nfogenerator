37fb3ccb939f91c53f3d4aef0ffc3f87ff5b7cbd
mteodorescu <mteodorescu@EN614592>
2017-10-12 11:15:43 +0300

    Add modernizr package and bundleconfig

--- NFOGenerator/Global.asax.cs
+++ NFOGenerator/Global.asax.cs
@@ -1,8 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using NFOGenerator.App_Start;
 using System.Web.Mvc;
+using System.Web.Optimization;
 using System.Web.Routing;
 
 namespace NFOGenerator
@@ -13,6 +11,7 @@ namespace NFOGenerator
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
