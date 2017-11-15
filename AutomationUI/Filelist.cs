b673a5d751789716d20eac8884fb59e372c3ff83
mteodorescu <mteodorescu@EN614592>
2017-11-15 10:10:08 +0200

    sample automation test

---
+++ AutomationUI/Filelist.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace AutomationUI
+{
+    [TestClass]
+    public class Filelist
+    {
+        private static IWebDriver _driver;
+
+        [AssemblyInitialize]
+        public static void Setup(TestContext testContext)
+        {
+            _driver = new ChromeDriver();
+        }
+
+        [TestMethod]
+        public void UploadFile()
+        {
+            _driver.Navigate().GoToUrl("https://filelist.ro/login.php");
+            _driver.FindElement(By.Id("username")).SendKeys("Megatron4FL");
+            _driver.FindElement(By.Id("password")).SendKeys("Automatica_2008^");
+            _driver.FindElement(By.Id("password")).SendKeys(Keys.Enter);
+
+            _driver.Navigate().GoToUrl("https://filelist.ro/upload.php");
+            _driver.FindElement(By.Name("name")).SendKeys("abracadabra");
+        }
+    }
+}
---
+++ AutomationUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AutomationUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutomationUI")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aac5105f-fbf6-4bd9-bc8c-465e7b48e020")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
