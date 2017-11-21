b5527e79eba0434b27ec6a09d3fe0d153dddcb38
Mihai <Mihai@DESKTOP-Acasa>
2017-11-21 20:39:23 +0200

    codex

--- Services/Parsers/CodexParser.cs
+++ Services/Parsers/CodexParser.cs
@@ -14,49 +14,51 @@ namespace Services.Parsers
 
         protected override string GetInstallNote()
         {
-            InfektExecutor exec = new InfektExecutor();
-            string data = GetNfoAsText();
+            return "- Extract\r\n- Burn or mount the.iso\r\n- Run setup.exe and install\r\n- Copy crack from CODEX dir to installdir\r\n-Play\r\n\r\n\r\nGeneral Notes:\r\n-Block the game's exe in your firewall to prevent the game from trying to go online ..\r\n-If you install games to your systemdrive, it may be necessary to run this game with admin privileges instead";
 
-            if (string.IsNullOrWhiteSpace(data))
-            {
-                return string.Empty;
-            }
+            //InfektExecutor exec = new InfektExecutor();
+            //string data = GetNfoAsText();
 
-            data = data.RemoveNonAscii().Trim();
+            //if (string.IsNullOrWhiteSpace(data))
+            //{
+            //    return string.Empty;
+            //}
 
-            Regex regex = new Regex(RegexInstallNotes);
-            MatchCollection matches = regex.Matches(data);
-            if (matches.Count > 0)
-            {
-                StringBuilder sb = new StringBuilder();
+            //data = data.RemoveNonAscii().Trim();
 
-                foreach (Match match in matches)
-                {
-                    var splits = match.Value.Split(new char[] { '\r', '\n' });
-                    foreach(string split in splits)
-                    {
-                        if (split.StartsWith("-"))
-                        {
-                            sb.Append(split);
-                        } else
-                        {
-                            if (split.IndexOf("general notes", StringComparison.OrdinalIgnoreCase) > -1)
-                            {
-                                sb.Append("\r\n\r\n\r\n");
-                                sb.Append("General notes: \r\n");
-                            }
-                            else
-                            {
-                                sb.Append($" {split.Trim(new char[] { '\r', '\n' })}\r\n");
-                            }
-                        }
-                    }
-                }
+            //Regex regex = new Regex(RegexInstallNotes);
+            //MatchCollection matches = regex.Matches(data);
+            //if (matches.Count > 0)
+            //{
+            //    StringBuilder sb = new StringBuilder();
 
-                return sb.ToString();
-            }
+            //    foreach (Match match in matches)
+            //    {
+            //        var splits = match.Value.Split(new char[] { '\r', '\n' });
+            //        foreach(string split in splits)
+            //        {
+            //            if (split.StartsWith("-"))
+            //            {
+            //                sb.Append(split);
+            //            } else
+            //            {
+            //                if (split.IndexOf("general notes", StringComparison.OrdinalIgnoreCase) > -1)
+            //                {
+            //                    sb.Append("\r\n\r\n\r\n");
+            //                    sb.Append("General notes: \r\n");
+            //                }
+            //                else
+            //                {
+            //                    sb.Append($" {split.Trim(new char[] { '\r', '\n' })}\r\n");
+            //                }
+            //            }
+            //        }
+            //    }
 
-            return string.Empty;
+            //    return sb.ToString();
+            //}
+
+            //return string.Empty;
         }
     }
 }
