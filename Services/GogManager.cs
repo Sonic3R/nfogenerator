11604929ca442bc47946fb57b866e7c2bf444f50
Mihai <Mihai@DESKTOP-Acasa>
2017-11-28 18:47:48 +0200

    GoG

---
+++ Services/GogManager.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using Services.Model;
+using System;
+using System.Net.Http;
+
+namespace Services
+{
+    public static class GogManager
+    {
+        private const string API_URL = "http://api.gog.com/products?ids={0}";
+
+        /// <summary>
+        /// Load game by id
+        /// </summary>
+        /// <param name="gogId"></param>
+        /// <returns></returns>
+        public static GogModel LoadGameById(string gogId)
+        {
+            if (string.IsNullOrWhiteSpace(gogId))
+            {
+                throw new ArgumentNullException(nameof(gogId));
+            }
+
+            Uri uri = new Uri(string.Format(API_URL, gogId));
+            using (HttpClient cl = new HttpClient())
+            {
+                string data = cl.GetStringAsync(uri).Result;
+
+                string firstElem = string.Format("\"{0}\"", gogId) + ":{";
+                string modifiedData = data.Replace(firstElem, "");
+                modifiedData = modifiedData.Substring(0, modifiedData.Length - 1);
+
+                return JsonConvert.DeserializeObject<GogModel>(modifiedData);
+            }
+        }
+    }
+}
--- Services/Model/GogModel.cs
+++ Services/Model/GogModel.cs
@@ -1,80 +1,106 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace Services.Model
 {
     public sealed class GogModel
     {
-
         public Data[] DataList { get; set; }
 
         public class Data
         {
-            public int id { get; set; }
-            public string title { get; set; }
-            public string purchase_link { get; set; }
-            public string slug { get; set; }
-            public Content_System_Compatibility content_system_compatibility { get; set; }
-            public Languages languages { get; set; }
-            public Links links { get; set; }
-            public In_Development in_development { get; set; }
-            public bool is_secret { get; set; }
-            public bool is_installable { get; set; }
-            public string game_type { get; set; }
-            public bool is_pre_order { get; set; }
-            public DateTime release_date { get; set; }
-            public Images images { get; set; }
-            public object[] dlcs { get; set; }
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("title")]
+            public string Title { get; set; }
+
+            [JsonProperty("purchase_link")]
+            public string Purchase_link { get; set; }
+
+            [JsonProperty("slug")]
+            public string Slug { get; set; }
+
+            [JsonProperty("content_system_compatibility")]
+            public Content_System_Compatibility Content_system_compatibility { get; set; }
+
+            [JsonProperty("in_development")]
+            public In_Development In_development { get; set; }
+
+            [JsonProperty("is_secret")]
+            public bool Is_secret { get; set; }
+
+            [JsonProperty("is_installable")]
+            public bool Is_installable { get; set; }
+
+            [JsonProperty("game_type")]
+            public string Game_type { get; set; }
+
+            [JsonProperty("release_date")]
+            public DateTime Release_date { get; set; }
+
+            [JsonProperty("id")]
+            public Images Images { get; set; }
         }
 
         public class Content_System_Compatibility
         {
-            public bool windows { get; set; }
-            public bool osx { get; set; }
-            public bool linux { get; set; }
-        }
+            [JsonProperty("windows")]
+            public bool Windows { get; set; }
 
-        public class Languages
-        {
-            public string cz { get; set; }
-            public string de { get; set; }
-            public string en { get; set; }
-            public string es { get; set; }
-            public string fr { get; set; }
-            public string hu { get; set; }
-            public string it { get; set; }
-            public string pl { get; set; }
-            public string ru { get; set; }
-        }
+            [JsonProperty("osx")]
+            public bool Osx { get; set; }
 
-        public class Links
-        {
-            public string purchase_link { get; set; }
-            public string product_card { get; set; }
-            public string support { get; set; }
-            public string forum { get; set; }
+            [JsonProperty("linux")]
+            public bool Linux { get; set; }
         }
 
         public class In_Development
         {
-            public bool active { get; set; }
-            public object until { get; set; }
+            [JsonProperty("active")]
+            public bool Active { get; set; }
+
+            [JsonProperty("until")]
+            public object Until { get; set; }
         }
 
         public class Images
         {
-            public string background { get; set; }
-            public string logo { get; set; }
-            public string logo2x { get; set; }
-            public string icon { get; set; }
-            public string sidebarIcon { get; set; }
-            public string sidebarIcon2x { get; set; }
-            public string menuNotificationAv { get; set; }
-            public string menuNotificationAv2 { get; set; }
+            [JsonProperty("background")]
+            public string Background { get; set; }
+
+            [JsonProperty("logo")]
+            public string Logo { get; set; }
+
+            [JsonProperty("logo2x")]
+            public string Logo2x { get; set; }
+
+            [JsonProperty("icon")]
+            public string Icon { get; set; }
         }
 
+        public class Dlcs
+        {
+            [JsonProperty("products")]
+            public Product[] Products { get; set; }
+
+            [JsonProperty("all_products_url")]
+            public string All_products_url { get; set; }
+
+            [JsonProperty("expanded_all_products_url")]
+            public string Expanded_all_products_url { get; set; }
+        }
+
+        public class Product
+        {
+            [JsonProperty("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("link")]
+            public string Link { get; set; }
+
+            [JsonProperty("expanded_link")]
+            public string Expanded_link { get; set; }
+        }
     }
 }
